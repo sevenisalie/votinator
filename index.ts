18939cc282813b3d5c8980a89c692c4a81aa8f51
sevenisalie <nolan.e.terry@gmail.com>
2023-04-15 14:17:49 -0500

    initial

---
+++ index.ts
@@ -0,0 +1,28 @@
+import puppeteer from "puppeteer"
+
+class Votinator {
+    constructor(parameters: Object) {
+
+    }
+
+    //get page object
+    // get vote input element
+    //make function to inject Jacob Greve - Niteowl as target.value - STEP 1
+    //make function to arrayify the csv of people
+    // make function that loops over array of people and answers the form with single person- STEP 2
+    // make function that submits vote and disposes page object
+
+    //repeat
+}
+
+type PageProps = {
+    url: string,
+    browser: string
+}
+class Page {
+
+    constructor(parameters:) {
+        parameters.url
+    }
+}
+
